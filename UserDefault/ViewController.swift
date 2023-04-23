da3992b33063b83888651e9b132ad8591f2d29de
havvk01 <90330806+havvk01@users.noreply.github.com>
2023-04-23 17:06:58 +0300

    Initial Commit

---
+++ UserDefault/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserDefault
+//
+//  Created by Slava Havvk on 23.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
